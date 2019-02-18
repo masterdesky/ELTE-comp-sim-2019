6408b0d08337c61bc2de2411e05350fa4b71afa3
masterdesky <masterdesky@gmail.com>
2019-02-19 00:57:59 +0100

    initial commit of simple pendulum

---
+++ 2. Simple Pendulum/src/comp-gl.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+clang-cl -Wall -o ..\Release\pendulum-gl.exe pendulum-gl.cpp
+ECHO Compiling Done!
---
+++ 2. Simple Pendulum/src/comp.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+clang-cl -Wall -o ..\Release\pendulum.exe pendulum.cpp
+ECHO Compiling Done!
