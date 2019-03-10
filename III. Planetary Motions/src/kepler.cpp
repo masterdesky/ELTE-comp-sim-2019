ae1efd27df2835892cb067a71ce75df7e47c232a
masterdesky <masterdesky@gmail.com>
2019-03-10 10:40:14 +0100

    corrected code

--- III. Planetary Motions/src/kepler.cpp
+++ III. Planetary Motions/src/kepler.cpp
@@ -7,24 +7,23 @@
 #include "odeint.hpp"
 
 const double pi = 4 * atan(1.0);
-const double GmPlusM = 4 * pi * pi;
-const double M_sun = 1;                 // Normalized mass of Sun
+const double GMPlusm = 4 * pi * pi;         // Kepler's Third Law: G(M + m)/(4*pi^2) = const. [AU^3/day^2]
+const double G = 6.67408 * pow(10, -11);    // Gravitational constant [m^3 * kg^-1 * s^-2]
+const double M_Sun = 1;                     // Normalized mass of Sun [M_Sun kg]
 
-static double r_ap;
-static double eccentricity;
-static double a;
-static double T;
-static double v0;
-static double plotting_years;
-static double dt;
-static double accuracy;
+static double r_ap;                         // Aphelion distance in AU [AU]
+static double eccentricity;                 // Eccentricity
+static double a;                            // Length of semi-major axis [AU]
+static double v0;                           // Initial velocity (tangential along y-axis) [AU/]
+static double plotting_years;               // Number of calculated years [Year]
+static double dt;                           // Step size [Year]
+static double accuracy;                     // Adaptive accuracy of simulation
 
-bool switch_t_with_y = false;           //  to interpolate to y = 0
+bool switch_t_with_y = false;               // To interpolate to y = 0
 
-// Calculate potential energy
-
-void potential_energy(const cpl::Vector& x) {
-    return ;
+//  Calculate potential energy
+double potential_energy(const cpl::Vector& x) {
+    return M_Sun * G / (sqrt(pow(x[1], 2) + pow(x[2], 2)));
 }
 
 
@@ -37,8 +36,8 @@ cpl::Vector derivates(const cpl::Vector& x) {
     f[0] = 1;
     f[1] = v_x;
     f[2] = v_y;
-    f[3] = - GmPlusM * r_x / rCubed;
-    f[4] = - GmPlusM * r_y / rCubed;
+    f[3] = - GMPlusm * r_x / rCubed;
+    f[4] = - GMPlusm * r_y / rCubed;
     if(switch_t_with_y) {
         //  use y as independent variable
         for(int i = 0; i < 5; i++) {
@@ -47,6 +46,7 @@ cpl::Vector derivates(const cpl::Vector& x) {
     }
     return f;
 }
+
 
 //  Change independent variable from t to y and step back to y = 0
 void interpolate_crossing(cpl::Vector x, int& crossing) {
@@ -62,15 +62,15 @@ int main(int argc, char* argv[]) {
     std::cout << " Kepler orbit comparing fixed and adaptive Runge-Kutta\n"
               << " -----------------------------------------------------\n";
     
-    std::string fixed_or_not = argv[1];         //  Fixed or adaptive
-    r_ap = atof(argv[2]);                       //  Aphelion distance in AU
-    eccentricity = atof(argv[3]);               //  Eccentricity
-    plotting_years = atof(argv[4]);             //  Number of calculated years
-    dt = atof(argv[5]);                         //  Step size
-    accuracy = atof(argv[6]);                   //  Adaptive accuracy of simulation
+    std::string fixed_or_not = argv[1];         // Fixed or adaptive
+    r_ap = atof(argv[2]);                       // Aphelion distance in AU
+    eccentricity = atof(argv[3]);               // Eccentricity
+    plotting_years = atof(argv[4]);             // Number of calculated years
+    dt = atof(argv[5]);                         // Step size
+    accuracy = atof(argv[6]);                   // Adaptive accuracy of simulation
 
-    a = r_ap / (1 + eccentricity);              //  Length of semi-major axis
-    v0 = sqrt(GmPlusM * (2 / r_ap - 1 / a));    //  Initial velocity (tangential along y-axis)
+    a = r_ap / (1 + eccentricity);              // Length of semi-major axis
+    v0 = sqrt(GMPlusm * (2 / r_ap - 1 / a));    // Initial velocity (tangential along y-axis)
 
     //  Initial parameters
     //  x0[0]: time; x0[1]: x coordinate; x0[2]: y coordinate; x0[3]: x velocity; x0[4]: y velocity
@@ -78,9 +78,10 @@ int main(int argc, char* argv[]) {
     x0[0] = 0;  x0[1] = r_ap;  x0[2] = 0;  x0[3] = 0;  x0[4] = v0;
 
     //  Changing variables
-    std::ofstream dataFile;     //  Datafile for outputs
-    cpl::Vector x;              //  Storing orbit parameters for every step
-    int steps, crossing;        //  Stepsize, Interpolate crossing
+    std::ofstream dataFile;     // Datafile for outputs
+    cpl::Vector x;              // Storing orbit parameters for every step
+    int steps, crossing;        // Stepsize, Interpolate crossing
+
 
     //
     //  FIXED STEP SIZE
@@ -97,7 +98,7 @@ int main(int argc, char* argv[]) {
             for(int i = 0; i < 5; i++) {
                 dataFile << x[i] << '\t';
             }
-            dataFile << '\n';
+            dataFile << potential_energy(x) << '\n';
             double y = x[2];
             cpl::RK4Step(x, dt, derivates);
             steps++;
@@ -105,7 +106,7 @@ int main(int argc, char* argv[]) {
                 interpolate_crossing(x, crossing);
             }
 
-        } while (x[0] <= plotting_years);
+        } while (x[0] < plotting_years);
 
         std::cout << " number of fixed size steps = " << steps << std::endl;
         std::cout << " data in file fixed.dat" << std::endl;
@@ -130,7 +131,7 @@ int main(int argc, char* argv[]) {
                 dataFile << x[i] << '\t';
             }
 
-            dataFile << '\n';
+            dataFile << potential_energy(x) << '\n';
             double t_save = x[0];
             double y = x[2];
             cpl::adaptiveRK4Step(x, dt, accuracy, derivates);
@@ -145,7 +146,7 @@ int main(int argc, char* argv[]) {
             if(y * x[2] < 0) {
                 interpolate_crossing(x, crossing);
             }
-        } while (x[0] <= plotting_years);
+        } while (x[0] < plotting_years);
 
         std::cout << " number of adaptive steps = " << steps << std::endl;
         std::cout << " step size: min = " << dt_min << "  max = " << dt_max << std::endl;
