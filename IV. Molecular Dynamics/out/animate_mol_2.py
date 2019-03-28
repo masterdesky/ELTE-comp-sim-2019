ff82914d39678bf851788b292e3d205a0f71cc3a
masterdesky <masterdesky@gmail.com>
2019-03-28 13:29:53 +0100

    velocity distribution animation

--- IV. Molecular Dynamics/out/animate_mol_2.py
+++ IV. Molecular Dynamics/out/animate_mol_2.py
@@ -15,6 +15,7 @@ import mpl_toolkits.mplot3d.art3d as art3d
 sns.set_style(style='whitegrid')
 # Others
 steps = 1
+dpi = 80
 fps = 40
 
 
@@ -23,16 +24,11 @@ def ANIMATE_VIDEO(path, video_path, gen_video_title, packages):
     video_title = video_path + gen_video_title
     
     nrows=1
-    ncols=2
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols*20,nrows*20), subplot_kw={'projection': '3d'})
+    ncols=1
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols*20,nrows*20))
 
     axislabelsize = 30
     labelpad = 20
-
-    # Plot a trail of a planet position for the last trail_secs seconds.
-    trail_secs = 5
-    # This corresponds to max_trail time points.
-    max_trail = int(trail_secs * fps)
 
     # Measuring time for progress bar
     starttime = datetime.now()
@@ -44,48 +40,19 @@ def ANIMATE_VIDEO(path, video_path, gen_video_title, packages):
     def animation(i):
 
         # Axis labels
-        axes.set_xlabel('Distance (x)', fontsize=axislabelsize, labelpad=labelpad)
-        axes.set_ylabel('Distance (y)', fontsize=axislabelsize, labelpad=labelpad)
-        axes.set_zlabel('Distance (z)', fontsize=axislabelsize, labelpad=labelpad)
+        axes.set_xlabel('Total velocities', fontsize=axislabelsize, labelpad=labelpad)
+        axes.set_ylabel('DIstribution of velocities', fontsize=axislabelsize, labelpad=labelpad)
 
         axes.tick_params(axis='both', which='major', labelsize=axislabelsize)
         
         # Centre the image on the fixed anchor point, and ensure the axes are equal
-        axes.set_xlim(0,L)
-        axes.set_ylim(0,L)
-        axes.set_zlim(0,L)
-        axes.set_aspect('equal', adjustable='box')
+        axes.set_ylim(0,0.4)
+        #axes.set_aspect('equal', adjustable='box')
 
-        # TRAILING EFFECT FROM: https://scipython.com/blog/the-double-pendulum/
-        # The trail will be divided into ns segments and plotted as a fading line.
-        ns = 20
-        s = max_trail // ns
-
-        for j in range(ns):
-            imin = i - (ns-j)*s
-            if imin < 0:
-                imin = 0
-            imax = imin + s + 1
-            # The fading looks better if we square the fractional length along the trail
-            alpha = (j/ns)**2
-
-            for k in range(0, (data_set.shape[1]-1)//9):
-                # Molecule trajectories
-                if sys.argv[1] == '3':
-                    axes.scatter(data_set[::steps,k*9][imin:imax],
-                                 data_set[::steps,k*9+1][imin:imax],
-                                 data_set[::steps,k*9+2][imin:imax], color=colors[k], alpha=alpha, s=10)
-
-                else:
-                    axes.scatter(data_set[::steps,k*9][imin:imax],
-                                 data_set[::steps,k*9+1][imin:imax],
-                                 data_set[::steps,k*9+2][imin:imax], color=colors[k], alpha=alpha, s=20)
-
-                # Molecules itself
-                axes.scatter(data_set[::steps,k*9][i],
-                             data_set[::steps,k*9+1][i],
-                             data_set[::steps,k*9+2][i], color='red', s=200)
-
+        velocities = np.array([np.sqrt(data_set[::steps][i][k*9]**2 +
+                                    data_set[::steps][i][k*9+1]**2 +
+                                    data_set[::steps][i][k*9+2]**2) for k in range(0, (data_set.shape[1]-1)//9)])
+        sns.distplot(velocities, ax=axes)
 
         fig.tight_layout()
 
@@ -93,7 +60,7 @@ def ANIMATE_VIDEO(path, video_path, gen_video_title, packages):
             # Don't show axes, only white background
             #axes.axis('off')
 
-        plt.savefig(path + '_img{0:4d}.png'.format(i), dpi=80)    # Save next frame as png
+        plt.savefig(path + '_img{0:4d}.png'.format(i), dpi=dpi)    # Save next frame as png
         image = imageio.imread(path + '_img{0:4d}.png'.format(i)) # Load saved image
         writer.append_data(image)                                 # Append this image as the next frame to video
 
@@ -122,20 +89,6 @@ if sys.argv[1] == '2':
 if sys.argv[1] == '3':
     data_set = np.genfromtxt('md3.dat')
 
-# Some simulation parameters
-if sys.argv[1] == '1':
-    L = 10
-
-if sys.argv[1] == '2':
-    N=64
-    rho=0.95
-    L = pow(N / rho, 1.0/3)
-
-if sys.argv[1] == '3':
-    N=64
-    rho=0.95
-    L = pow(N / rho, 1.0/3)
-
 # Limits of animated parts
 MIN_LIM = int(sys.argv[2])
 MAX_LIM = int(sys.argv[3])
@@ -148,18 +101,5 @@ if MIN_LIM > MAX_LIM:
     MIN_LIM_TEMP = MIN_LIM
     MIN_LIM = MAX_LIM
     MAX_LIM = MIN_LIM_TEMP
-
-
-# Generate new colors
-if sys.argv[5] == 'newcolors':
-    # Define randomized colormaps for particles
-    colors = np.empty(((data_set.shape[1]-1)//3,3))
-    for i in range(0,len(colors)):
-        colors[i] = np.array([random.random(), random.random(), random.random()])
-
-    np.savetxt('colors.dat', colors, delimiter=' ')
-
-else:
-    colors = np.genfromtxt('colors.dat')
 
 ANIMATE_VIDEO(path = '.\\frames\\', video_path='.\\videos\\', gen_video_title=sys.argv[4], packages=20)
