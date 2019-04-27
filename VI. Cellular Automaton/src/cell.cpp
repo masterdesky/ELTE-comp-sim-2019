886806217d29d942df7c626a1bf613e6ef6ea460
masterdesky <masterdesky@gmail.com>
2019-04-28 01:40:11 +0200

    cellular automaton init commit

---
+++ VI. Cellular Automaton/src/cell.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+std::string boundary_condition;
+
+int main(int argc, char* argv[]) {
+    std::cout << "Conway's Cell Automaton\n"
+              << " ----------------------\n";
+    
+    boundary_condition = argv[1];                   // Choosen boundary condition for the game
+    
+    
+
+
+
+
+}
