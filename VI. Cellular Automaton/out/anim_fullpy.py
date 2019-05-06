078a7655d8bf0f48c1980f92311b739e18c22b29
masterdesky <masterdesky@gmail.com>
2019-05-07 01:13:23 +0200

    ca finished

--- VI. Cellular Automaton/out/anim_fullpy.py
+++ VI. Cellular Automaton/out/anim_fullpy.py
@@ -17,20 +17,24 @@ class SandpileAnimation():
 
         self.test = True
         
-        # INPUT DATA SHOULD BE AN ARRAY, SO WE CONVERT INTO IT FROM A LIST
+        # Input data should be an array, so make sure, init_sandpile returns with that
+        # Also save the current configuration
         self.starting_points = np.asarray(self.init_sandpile(width=self.width, height=self.height))
         self.current_state = self.starting_points
 
-        # CREATE A FULL NULL-MATRIX IN THE SIZE OF THE FULL ARRAY
+        # Create a full-zero background for the initialization
         self.Background = np.zeros_like(self.starting_points)
 
-        # Set up a borderless frame
+        # Set up a borderless frame for the sandpile
         self.ax = self.game_arena.add_axes([0, 0, 1, 1], xticks=[], yticks=[], frameon=False)
 
-        # FROM STACKOVERFLOW, PLOTTING BINARY MATRIX WITH WHITE AND BLACK SQUARES
-        # ON STACKOVERFLOW IT WAS AN EXAMPLE FOR ANIMATING A FRACTAL
+        # Defining the image frame, shown in the simulation
         self.Image = self.ax.imshow(self.starting_points, cmap='Greys', interpolation='nearest')
 
+
+    #
+    # Actual Abelian-sandpile mechanics
+    #
     def init_sandpile(self, width, height):
     
         sandpile = np.zeros((height+2, width+2))
@@ -60,8 +64,7 @@ class SandpileAnimation():
         
         return(sandpile)
 
-    # WE SHOULD INITIALIZE THE PLOTTING ANIMATION WITH A BACKGROUND FIRST IN MATPLOTLIB'S FuncAnimation
-    # THIS IS NECESSARY, BECAUSE THE ANIMATION WOULD ONLY SHOW ONLY ONE FRAME, AND WE DON'T WANT IT
+    # Initializing the frame for the FuncAnimations
     def init_anim(self):
         self.Image.set_data(self.Background)
         return self.Image,
@@ -71,25 +74,33 @@ class SandpileAnimation():
         self.Image.set_data(self.starting_points[1:self.width+1,1:self.height+1])
         self.starting_points = np.asarray(self.step_sandpile(sandpile=self.starting_points, width=self.width, height=self.height))
 
-        # Update current state, if there are changes in the sandpile, else stop
+        #
+        # Check if there are changes in the sandpile compared to the previous
+        # state. If there are, then continue with the process, else stop.
+        #
+        # First set the checking to False, initially consider a static position.
+        self.test = False
         for i in range(1, self.height+1):
             for j in range(1, self.width+1):
+                # If a deviation is found, se the checking state to True to
+                # indicate, there are deviations. In this case, jump out from
+                # both of the loops.
                 if(self.starting_points[i][j] != self.current_state[i][j]):
                     self.test = True
                     break
             if(self.test == True):
                 break
 
+        # If there are any deviation, continue to the next step, and save
+        # the current configuration for the check in the next step.
         if(self.test == True):
             self.current_state = self.starting_points
 
-        return self.Image,
+        # If there aren't any deviation from the previous state, stop and save the video.
+        else:
+            self.anim.event_source.stop()
 
-    def gen(self):
-        i = 0
-        while(self.test == True):
-            i += 1
-            yield i
+        return self.Image,
 
     def animate(self):
 
@@ -99,7 +110,7 @@ class SandpileAnimation():
 
         # Animate the function
         #self.starting_points = self.starting_points
-        self.anim = animation.FuncAnimation(fig=self.game_arena, func=self.generation_update, frames=self.gen, interval=50, init_func=self.init_anim, blit=True)
+        self.anim = animation.FuncAnimation(fig=self.game_arena, func=self.generation_update, frames=None, interval=50, init_func=self.init_anim, blit=True)
         self.anim.save('.\\videos\\' + self.video_name + '.mp4', writer=self.writer)
 
 # MAIN
